a4004a0a567192527924a7bce22d4a80d8719a29
saurabh-2905 <saurabhband2905@gmail.com>
2024-04-16 17:00:31 +0200

    tool for labeling test data

---
+++ detection method/labelling tool.py
@@ -0,0 +1,147 @@
+import json
+import tkinter as tk
+from tkinter import messagebox
+
+# Create root window
+root = tk.Tk()
+
+# To map the index to the timestamp
+timestamps = []
+
+# Get trace file to calculate timestamps from index
+def generate_mapper():
+    trace_path = tracefile_path.get()
+    trace = read_traces(trace_path)
+    for _, time in trace:
+        timestamps.append(time)
+
+    for i, (field, entry) in enumerate(metadata_entries.items()):
+        if field == 'path':
+            entry.insert(0, tracefile_path.get())
+        else:
+            i_new = i + 2
+            entry.insert(0, tracefile_path.get().split('/')[-i_new])
+
+    for field, entry in label_entries.items():
+        if field == 'file_name':
+            entry.insert(0, tracefile_path.get().split('/')[-1])
+            
+
+# Get path to trace file
+frame = tk.Frame(root)
+frame.pack()
+label = tk.Label(frame, text='path to trace file')
+label.pack(side='left')
+entry = tk.Entry(frame)
+entry.pack(side='right')
+tracefile_path = entry
+
+# Create save label button
+generate_mapper_button = tk.Button(root, text='Generate Mapper', command=generate_mapper)
+generate_mapper_button.pack()
+
+
+metadata_entries = {}
+for i, field in enumerate(['behaviour', 'version','thread', 'code', 'path']):
+    i_new = i + 2
+    print(tracefile_path.get())
+    frame = tk.Frame(root)
+    frame.pack()
+    label = tk.Label(frame, text=field)
+    label.pack(side='left')
+    # entry = tk.Entry(frame)
+    # metadata_entries[field] = entry
+    entry = tk.Entry(frame)
+    entry.pack(side='right')
+    metadata_entries[field] = entry
+
+# Create label entries
+label_entries = {}
+for field in ['file_name', 'index1', 'index2', 'class']:
+    frame = tk.Frame(root)
+    frame.pack()
+    label = tk.Label(frame, text=field)
+    label.pack(side='left')
+    entry = tk.Entry(frame)
+    entry.pack(side='right')
+    label_entries[field] = entry
+
+# Create labels list
+labels = {}
+trace = None
+
+def read_traces(trace_path):
+    '''
+    read the trace files and extract variable names
+    trace_path: path to the trace files -> str
+
+    return:
+    data = [ [event, timestamp], [], [],......,[] ]
+    '''
+    with open(trace_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+
+def calculate_timestamps(index1, index2):
+    index1 = int(index1)
+    index2 = int(index2)
+    timestamp1 = timestamps[index1]
+    timestamp2 = timestamps[index2]
+    return timestamp1, timestamp2
+
+# Function to save label
+def save_label():
+    file_name = label_entries['file_name'].get()
+    label = tuple(entry.get() for field, entry in label_entries.items() if field != 'file_name')
+    print(label)
+    timestamp1, timestamp2 = calculate_timestamps(label[0], label[1])
+    label = (int(label[0]), int(label[1]), timestamp1, timestamp2, int(label[2]))    ### get the labels in format (index1, index2 timestamp1, timestamp2, class)
+    if file_name in labels:
+        labels[file_name].append(label)
+    else:
+        labels[file_name] = [label]
+    for field, entry in label_entries.items():
+        if field != 'file_name' and field != 'class':   ### do not erase content of file_name so that we can store multiple labels for same file name
+            entry.delete(0, 'end')
+
+# Create save label button
+save_label_button = tk.Button(root, text='Save label', command=save_label)
+save_label_button.pack()
+
+# Function to save data
+def save_data():
+    # Get metadata
+    metadata = {field: entry.get() for field, entry in metadata_entries.items()}
+
+    # Create data
+    data = {
+        'metadata': metadata,
+        'labels': labels
+    }
+
+    # Write data to JSON file
+    write_path = metadata_entries['path'].get().split('/')[-1]
+    with open(f'{write_path}_label.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+    # Show success message
+    messagebox.showinfo('Success', f'Data saved to {write_path}_labels.json')
+
+    # Clear all entries
+    for field, entry in label_entries.items():
+        entry.delete(0, 'end')
+
+    for field, entry in metadata_entries.items():
+        entry.delete(0, 'end')
+
+    metadata = {}
+    labels = {}
+
+# Create save button
+save_button = tk.Button(root, text='Save data', command=save_data)
+save_button.pack()
+
+# Start main loop
+root.mainloop()
